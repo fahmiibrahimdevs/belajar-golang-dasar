5ec3a05a51db4beb9803862e1da98ef581920573
fahmiibrahimdevs <midragon.project@gmail.com>
2025-05-30 13:09:50 +0700

    update string.go > add len

--- string.go
+++ string.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Fahmi")
-	fmt.Println("Fahmi Ibrahim")
-	fmt.Println("Fahmi Ibrahim Nexaryn")
+	fmt.Println(len("Fahmi"))
+	fmt.Println("Fahmi Ibrahim"[0])
+	fmt.Println("Fahmi Ibrahim Nexaryn"[1])
 }
