3f7b9ab852f942f98d252293091bd2a1eb6e93b9
fahmiibrahimdevs <midragon.project@gmail.com>
2025-05-30 13:15:34 +0700

    add variable.go

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Fahmi Ibrahim"
+	fmt.Println(name)
+
+	name = "Fahmi Nexaryn"
+	fmt.Println(name)
+
+	var (
+		firstName = "Fahmi"
+		lastName = "Ibrahim"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
